e736496668932d65e7fbb4dc466dc1a036f9c6d8
Jindae <jindae.kim@seoultech.ac.kr>
2026-09-08 04:50:12 +0900

    Unreliable code example.

---
+++ Example.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+
+    public static void main(String[] args) {
+        Example e = new Example();
+        String s = e.reverse("abc", new ArrayList<>());        
+        System.out.println(s);
+        s = e.reverse("", new ArrayList<>());
+        System.out.println(s);
+        s = e.reverse(null, new ArrayList<>());
+        System.out.println(s);
+        s = e.reverse("xyz", null);
+        System.out.println(s);
+    }
+
+    public String reverse(String str, List<String> list) {
+        if(list == null)
+            list = new ArrayList<>();
+        if (str.length() < 0) {  
+            StringBuffer sb = new StringBuffer();
+            for (String s : list) {
+                sb.append(s);
+            }
+            return sb.toString();          
+            
+        }
+        list.add(str.substring(str.length()-1));
+        return reverse(str.substring(0, str.length()-1), list);            
+    }
+}
