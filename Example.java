9e9f7947ef142a1c4e77aa716e1e923935e1d956
Lee Gyuhan <leegyuhan21@gmail.com>
2026-09-09 16:23:08 +0900

    Fix reverse

--- Example.java
+++ Example.java
@@ -5,22 +5,22 @@ public class Example {
 
     public static void main(String[] args) {
         Example e = new Example();
-        String s = e.reverse("abc", new ArrayList<>());        
-        System.out.println(s);        
+        String s = e.reverse("gyuhan-review", new ArrayList<>());
+        System.out.println(s);
     }
 
     public String reverse(String str, List<String> list) {
-        if(list == null)
+        if (list == null)
             list = new ArrayList<>();
-        if (str.length() < 0) {  
+        if (str.length() <= 0) {
             StringBuffer sb = new StringBuffer();
             for (String s : list) {
                 sb.append(s);
             }
-            return sb.toString();          
-            
+            return sb.toString();
+
         }
-        list.add(str.substring(str.length()-1));
-        return reverse(str.substring(0, str.length()-1), list);            
+        list.add(str.substring(str.length() - 1));
+        return reverse(str.substring(0, str.length() - 1), list);
     }
 }
